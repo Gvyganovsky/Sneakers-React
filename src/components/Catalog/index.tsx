ee1f4f4a592722603294aeda6b053c0024b66022
gvyganovsky <gvyganovsky@gmail.com>
2024-01-18 16:28:38 +0300

    Search + Axios

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -3,7 +3,12 @@ import Product from '../Product';
 import React from 'react';
 
 function Catalog({ cartProducts, setCartProducts }: any) {
-    const [products, setProducts] = React.useState<any[]>([])
+    const [searchValue, setSearchValue] = React.useState('');
+    const onChangeSearchInput = (event: any) => {
+        setSearchValue(event.target.value)
+    }
+
+    const [products, setProducts] = React.useState<any[]>([]);
     React.useEffect(() => {
         fetch('https://65a7c5a394c2c5762da7817d.mockapi.io/products').then((res) => {
             return res.json();
@@ -14,14 +19,14 @@ function Catalog({ cartProducts, setCartProducts }: any) {
     }, []);
 
     const onAddToCart = (obj: any) => {
-        setCartProducts([...cartProducts, obj]);
+        setCartProducts((prev: any) => [...prev, obj]);
     };
 
     return (
         <section className={styles.catalog}>
             <div className={styles.catalog__container}>
                 <h2 className={styles.catalog__title}>
-                    Все кроссовки
+                    {searchValue ? 'Поиск по запросу "' + searchValue + '"' : 'Все кроссовки'}
                 </h2>
                 <form action="" className={styles.search__block}>
                     <img
@@ -34,18 +39,24 @@ function Catalog({ cartProducts, setCartProducts }: any) {
                     <input
                         placeholder='Поиск...'
                         type='search'
+                        value={searchValue}
+                        onChange={onChangeSearchInput}
                         className={styles.search__input}
+                        maxLength={24}
                     />
                 </form>
                 <div className={styles.catalog__content}>
-                    {products.map((product) => (
-                        <Product
-                            img={product.img}
-                            title={product.title}
-                            price={product.price}
-                            onAddToCart={(obj: any) => onAddToCart(obj)}
-                        />
-                    ))}
+                    {products
+                        .filter((product) => product.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((product) => (
+                            <Product
+                                key={product.title}
+                                img={product.img}
+                                title={product.title}
+                                price={product.price}
+                                onAddToCart={(obj: any) => onAddToCart(obj)}
+                            />
+                        ))}
                 </div>
             </div>
         </section >
