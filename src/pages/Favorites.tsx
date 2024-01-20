821f371467555e1a6862fb4a24fae6ba81c44e62
gvyganovsky <gvyganovsky@gmail.com>
2024-01-20 18:07:36 +0300

    Favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import styles from '../components/Catalog/Catalog.module.scss';
 import Product from "../components/Product";
+import Button from '../components/Button/';
 
 function Favorites({ setFavorite, favorite, setCartProducts }: any) {
     React.useEffect(() => {
@@ -10,12 +11,12 @@ function Favorites({ setFavorite, favorite, setCartProducts }: any) {
         });
     }, []);
 
-    const onAddFavorite = (obj: any) => {
+    const onAddFavorite = async (obj: any) => {
         if (favorite.find((obj: { id: any; }) => obj.id === obj.id)) {
             axios.delete(`https://65aa1b5e081bd82e1d961920.mockapi.io/favorite/${obj.id}`);
             setFavorite((prev: any) => prev.filter((product: { id: any; }) => product.id !== obj.id));
         } else {
-            axios.post('https://65aa1b5e081bd82e1d961920.mockapi.io/favorite', obj);
+            const resp = await axios.post('https://65aa1b5e081bd82e1d961920.mockapi.io/favorite', obj);
             setFavorite((prev: any) => [...prev, obj]);
         }
     }
@@ -31,22 +32,42 @@ function Favorites({ setFavorite, favorite, setCartProducts }: any) {
                 <h2 className={styles.catalog__title}>
                     Мои закладки
                 </h2>
-
-                <div className={styles.catalog__content}>
-                    {favorite.map((product: { title: any; img: any; price: any; }) => (
-                        <Product
-                            key={product.title}
-                            // img={product.img}
-                            // title={product.title}
-                            // price={product.price}
-                            onAddFavorite={() => onAddFavorite(product)}
-                            onAddToCart={(obj: any) => onAddToCart(obj)}
-                            favorited={true}
-                            {...product}
-                        />
-                    ))}
-                </div>
-            </div>
+                {
+                    favorite.length > 0 ? (
+                        <div className={styles.catalog__content}>
+                            {favorite.map((product: { title: any; img: any; price: any; }) => (
+                                <Product
+                                    key={product.title}
+                                    // img={product.img}
+                                    // title={product.title}
+                                    // price={product.price}
+                                    onAddFavorite={() => onAddFavorite(product)}
+                                    onAddToCart={(obj: any) => onAddToCart(obj)}
+                                    favorited={true}
+                                    {...product}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className={styles.nullFavorite}>
+                            <img
+                                src='/assets/icons/smileFav.svg'
+                                alt='smileFav'
+                                width={70}
+                                height={70}
+                                className={styles.nullFavorite__img}
+                            />
+                            <p className={styles.nullFavorite__title}>
+                                Закладок нет
+                            </p>
+                            <p className={styles.nullFavorite__text}>
+                                Вы ничего не добавляли в закладки
+                            </p>
+                            <Button text='Вернуться назад' className={styles.nullFavorite__btn} />
+                        </div>
+                    )
+                }
+            </div >
         </section >
     );
 }
